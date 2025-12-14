9090993c83afd5ed72882d491cd73bc95f91241e
Mahdi Mortazavi😎 <127998145+Mahdi-mortazavi@users.noreply.github.com>
2025-12-14 15:34:14 +0330

    Revert "feat: refactor project to modular BLoC architecture (#9)"
    This reverts commit 496070149956d4a32c6d062a69d0bdc598902919.

--- lib/features/task_management/domain/repositories/task_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:app/features/task_management/domain/entities/task.dart';
-
-abstract class TaskRepository {
-  Future<List<Task>> getTasks();
-  Future<void> saveTasks(List<Task> tasks);
-}
